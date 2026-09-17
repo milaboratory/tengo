a20a79cd6da0533a0004f7f00fbedeb61c709d46
xnacly <47723417+xnacly@users.noreply.github.com>
2026-09-17 12:26:54 +0200

    objects: preallocate small Int values

--- builtins.go
+++ builtins.go
@@ -311,17 +311,17 @@ func builtinLen(args ...Object) (Object, error) {
 	}
 	switch arg := args[0].(type) {
 	case *Array:
-		return &Int{Value: int64(len(arg.Value))}, nil
+		return NewInt(int64(len(arg.Value))), nil
 	case *ImmutableArray:
-		return &Int{Value: int64(len(arg.Value))}, nil
+		return NewInt(int64(len(arg.Value))), nil
 	case *String:
-		return &Int{Value: int64(len(arg.Value))}, nil
+		return NewInt(int64(len(arg.Value))), nil
 	case *Bytes:
-		return &Int{Value: int64(len(arg.Value))}, nil
+		return NewInt(int64(len(arg.Value))), nil
 	case *Map:
-		return &Int{Value: int64(len(arg.Value))}, nil
+		return NewInt(int64(len(arg.Value))), nil
 	case *ImmutableMap:
-		return &Int{Value: int64(len(arg.Value))}, nil
+		return NewInt(int64(len(arg.Value))), nil
 	default:
 		return nil, ErrInvalidArgumentType{
 			Name:     "first",
--- objects.go
+++ objects.go
@@ -1001,6 +1001,27 @@ type Int struct {
 	Value int64
 }
 
+const (
+	smallIntMin = -128
+	smallIntMax = 1023
+)
+
+var smallInts [smallIntMax - smallIntMin + 1]Int
+
+func init() {
+	for i := range smallInts {
+		smallInts[i].Value = int64(i) + smallIntMin
+	}
+}
+
+// NewInt returns an Int object for v.
+func NewInt(v int64) *Int {
+	if v >= smallIntMin && v <= smallIntMax {
+		return &smallInts[v-smallIntMin]
+	}
+	return &Int{Value: v}
+}
+
 func (o *Int) String() string {
 	return strconv.FormatInt(o.Value, 10)
 }
@@ -1018,70 +1039,37 @@ func (o *Int) BinaryOp(op token.Token, rhs Object) (Object, error) {
 		switch op {
 		case token.Add:
 			r := o.Value + rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Sub:
 			r := o.Value - rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Mul:
 			r := o.Value * rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Quo:
 			r := o.Value / rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Rem:
 			r := o.Value % rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.And:
 			r := o.Value & rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Or:
 			r := o.Value | rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Xor:
 			r := o.Value ^ rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.AndNot:
 			r := o.Value &^ rhs.Value
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Shl:
 			r := o.Value << uint64(rhs.Value)
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Shr:
 			r := o.Value >> uint64(rhs.Value)
-			if r == o.Value {
-				return o, nil
-			}
-			return &Int{Value: r}, nil
+			return NewInt(r), nil
 		case token.Less:
 			if o.Value < rhs.Value {
 				return TrueValue, nil
